bcd49b8c645b8991352688dc4e2d1c94cf0bab97
NataliaCarolineLopez <nataliacarolinesenai@gmail.com>
2025-08-25 08:20:40 -0300

    Update agenda.php

--- agenda.php
+++ agenda.php
@@ -5,6 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agendamento - Salão de Beleza</title>
 
+      <h1>Agendamento - Salão de Beleza</h1>
+
+    <form action="{{ route('agendamentos.store') }}" method="POST">
+    <!-- Isso é necessário em formulários POST no Laravel -->
+
+        <label for="cliente">Nome do Cliente:</label>
+        <input type="text" name="cliente" id="cliente" placeholder="Digite seu nome" value="" required>
+
+        <label for="telefone">Telefone:</label>
+        <input type="tel" name="telefone" id="telefone" placeholder="(00) 00000-0000" value="" required>
+
+        <label for="servico">Serviço:</label>
+        <select name="servico" id="servico" required>
+            <option value="">Selecione</option>
+            <option value="Corte de Cabelo">Corte de Cabelo</option>
+            <option value="Pintura">Pintura</option>
+            <option value="Manicure e Pedicure">Manicure e Pedicure</option>
+            <option value="Escova">Escova</option>
+            <option value="Hidratação">Hidratação</option>
+        </select>
+
+        <label for="data">Data:</label>
+        <input type="date" name="data" id="data" value="" required>
+
+        <label for="hora">Hora:</label>
+        <input type="time" name="hora" id="hora" value="" required>
+
+        <button type="submit">Agendar</button>
+    </form>
+</body>
+</html>
+
     <style>
         * {
             margin: 0;
@@ -82,34 +114,3 @@
     </style>
 </head>
 <body>
-    <h1>Agendamento - Salão de Beleza</h1>
-
-    <form action="{{ route('agendamentos.store') }}" method="POST">
-    
-
-        <label for="cliente">Nome do Cliente:</label>
-        <input type="text" name="cliente" id="cliente" placeholder="Digite seu nome" value="" required>
-
-        <label for="telefone">Telefone:</label>
-        <input type="tel" name="telefone" id="telefone" placeholder="(00) 00000-0000" value="" required>
-
-        <label for="servico">Serviço:</label>
-        <select name="servico" id="servico" required>
-            <option value="">Selecione</option>
-            <option value="Corte de Cabelo">Corte de Cabelo</option>
-            <option value="Pintura">Pintura</option>
-            <option value="Manicure e Pedicure">Manicure e Pedicure</option>
-            <option value="Escova">Escova</option>
-            <option value="Hidratação">Hidratação</option>
-        </select>
-
-        <label for="data">Data:</label>
-        <input type="date" name="data" id="data" value="{{ old('data') }}" required>
-
-        <label for="hora">Hora:</label>
-        <input type="time" name="hora" id="hora" value="{{ old('hora') }}" required>
-
-        <button type="submit">Agendar</button>
-    </form>
-</body>
-</html>
